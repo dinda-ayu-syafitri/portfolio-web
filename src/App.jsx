f33a70818a7a484f9adb946074d269d04966cd2e
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-02-09 23:46:07 +0700

    Add : Page Sectioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
+};
 
 export default App;
