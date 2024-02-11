46b3de69678a5d623840764083e24b96d454ebbe
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-02-11 23:13:29 +0700

    Add : Hero section animated

--- src/modules/hero-section/HeroSection.jsx
+++ src/modules/hero-section/HeroSection.jsx
@@ -2,6 +2,11 @@ import { motion } from "framer-motion";
 import { AnimatedHeroImage } from "../../components/animatedHeroImage/AnimatedHeroImage";
 // import { ScrollingText } from "../../components/scrollingText/ScrollingText";
 export const HeroSection = () => {
+  const heroText = {
+    initial: { x: "-100vw" },
+    animate: { x: 0, transition: { duration: 1, staggerChildren: 0.1 } },
+  };
+
   const slideText = {
     initialToRight: { x: "-100vw" },
     initialToLeft: { x: "0vw" },
@@ -45,12 +50,23 @@ export const HeroSection = () => {
           Website Developer
         </h3>
       </motion.div>
-      <div className="flex justify-center flex-col gap-5 md:basis-2/3">
-        <h1 className="text-2xl md:text-3xl">HI, I&apos;M DINDA</h1>
-        <h1 className="font-jura text-theme-orange text-6xl md:text-8xl font-bold">
+      <motion.div
+        className="flex justify-center flex-col gap-5 md:basis-2/3"
+        variants={heroText}
+        initial={"initial"}
+        animate={"animate"}
+        // whileInView={"animate"}
+      >
+        <motion.h1 className="text-2xl md:text-3xl" variants={heroText}>
+          HI, I&apos;M DINDA
+        </motion.h1>
+        <motion.h1
+          className="font-jura text-theme-orange text-6xl md:text-8xl font-bold"
+          variants={heroText}
+        >
           Web Developer
-        </h1>
-      </div>
+        </motion.h1>
+      </motion.div>
       <div className="flex justify-center md:basis-1/3 w-full">
         {/* <img src="./hero-image.svg" alt="hero-image" /> */}
         <AnimatedHeroImage />
