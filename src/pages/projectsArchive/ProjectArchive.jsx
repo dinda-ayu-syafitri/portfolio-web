55b61d36fdf489c38b76544e2d064204237693e8
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-02-15 03:58:46 +0700

    fix : redirect bug in project archive page

--- src/pages/projectsArchive/ProjectArchive.jsx
+++ src/pages/projectsArchive/ProjectArchive.jsx
@@ -9,11 +9,11 @@ export const ProjectArchive = () => {
         <h1>Not Yet Available</h1>
         <p>Under Development</p>
         <p>Please contact me to get my projects archives for the meantime</p>
-        <Link to="/#contact">
+        <a href="/#contact">
           <button className="flex mx-auto justify-center items-center py-2 px-8 bg-theme-orange text-theme-off-white max-w-[90vw] md:max-w-[60vw] rounded-lg mt-5 hover:bg-theme-off-white hover:text-theme-orange">
             Send Me a Message
           </button>
-        </Link>
+        </a>
       </section>
     </MainLayout>
   );
