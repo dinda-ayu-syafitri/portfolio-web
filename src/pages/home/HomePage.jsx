e242054b2e87766720d6e18540a884cc38767c64
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-02-15 11:28:49 +0700

    fix : Hashed routing bugs

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HeroSection } from "../../modules/hero-section/HeroSection";
 import { AboutMe } from "../../modules/aboutMe-section/AboutMe";
 import { SkillsExpertise } from "../../modules/skills-expertise/SkillsExpertise";
 import { Projects } from "../../modules/projects/Projects";
 import { MainLayout } from "../../Layouts/MainLayout";
 import { Contact } from "../../modules/contact/Contact";
+import { useLocation } from "react-router-dom";
 
 export const HomePage = () => {
+  const hash = useLocation().hash;
+
+  useEffect(() => {
+    const section = document.querySelector(hash);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [hash]);
+
   return (
     <MainLayout>
       <HeroSection />
--- src/pages/projectsArchive/ProjectArchive.jsx
+++ src/pages/projectsArchive/ProjectArchive.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MainLayout } from "../../Layouts/MainLayout";
 import { Link } from "react-router-dom";
+import { Contact } from "../../modules/contact/Contact";
 
 export const ProjectArchive = () => {
   return (
@@ -9,11 +10,11 @@ export const ProjectArchive = () => {
         <h1>Not Yet Available</h1>
         <p>Under Development</p>
         <p>Please contact me to get my projects archives for the meantime</p>
-        <a href="/#contact">
+        <Link to="/#contact">
           <button className="flex mx-auto justify-center items-center py-2 px-8 bg-theme-orange text-theme-off-white max-w-[90vw] md:max-w-[60vw] rounded-lg mt-5 hover:bg-theme-off-white hover:text-theme-orange">
             Send Me a Message
           </button>
-        </a>
+        </Link>
       </section>
     </MainLayout>
   );
