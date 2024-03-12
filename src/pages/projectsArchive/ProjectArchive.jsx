8f46c7774472a049e2e00784ca6fb9b7e404fd0a
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2024-03-12 08:01:34 +0700

    Fix : Basic styling for project archive table

--- src/pages/projectsArchive/ProjectArchive.jsx
+++ src/pages/projectsArchive/ProjectArchive.jsx
@@ -44,12 +44,17 @@ export const ProjectArchive = () => {
   return (
     <MainLayout>
       <div className="flex flex-col text-center justify-center items-center pt-10 md:pt-20 mx-5 px-10 md:px-32" >
+        <div className="text-left w-full my-10">
+
+        <h1 className="font-bold text-4xl mb-3 text-theme-orange">Project Archive</h1>
+<p>These are several projects that I do, some of them are personal project, and some are team project</p>
+        </div>
         <table className="text-left">
         <thead>
           {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
+            <tr key={headerGroup.id} className="bg-theme-grey rounded-t-md">
               {headerGroup.headers.map(header => (
-                <th key={header.id}>
+                <th key={header.id} className="px-3 py-3 first:rounded-tl-md last:rounded-tr-md" >
                   {header.isPlaceholder
                     ? null
                     : flexRender(
@@ -63,9 +68,9 @@ export const ProjectArchive = () => {
         </thead>
         <tbody>
           {table.getRowModel().rows.map(row => (
-            <tr key={row.id}>
+            <tr key={row.id} className="border-b border-theme-off-white hover:bg-theme-orange hover:bg-opacity-10">
               {row.getVisibleCells().map(cell => (
-                <td key={cell.id}>
+                <td key={cell.id} className="px-3 py-3">
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               ))}
