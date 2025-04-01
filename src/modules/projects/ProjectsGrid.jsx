d3b2cdec2527c55d76b845ad748c0094aca70cf7
dinda-ayu-syafitri <dindasyafitri@gmail.com>
2025-04-02 00:02:52 +0700

    feat: add duakomalima project

--- src/modules/projects/ProjectsGrid.jsx
+++ src/modules/projects/ProjectsGrid.jsx
@@ -13,7 +13,7 @@ export const ProjectsGrid = () => {
       <div className="flex mx-[1vw] md:mx-[5vw] gap-2 md:gap-5 justify-start font-jura rounded-t-xl text-theme-dark-blue ">
         <div className="grid grid-cols-3 text-center justify-center w-full md:w-2/4 ">
           <div
-            className={`rounded-t-xl  py-5 z-30 ${
+            className={`rounded-t-xl  py-5 z-25 ${
               projectCategory === "App"
                 ? "bg-theme-off-white shadow-[10_-10px_30px_5px_rgba(0,0,0,0.3)]"
                 : "bg-theme-light-grey  "
@@ -47,6 +47,7 @@ export const ProjectsGrid = () => {
       <div className="mx-[2vw] md:mx-[5vw] md:px-10 pt-10 pb-10 grid grid-cols-1 md:grid-cols-3 gap-5 bg-theme-off-white rounded-b-xl rounded-r-xl shadow-[0_-5px_10px_5px_rgba(0,0,0,0.3)] z-50">
         {projectData
           .filter((project) => project.category.includes(projectCategory))
+          .sort((a, b) => b.id - a.id) // Sort by largest ID first
           .map((project) => (
             <GridProjectCard key={project.id} project={project} />
           ))}
